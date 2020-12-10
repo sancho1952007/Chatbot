9c8378d1d3b8bb792d13e060f7eccfde87427f35
sancho1952007 <73981314+sancho1952007@users.noreply.github.com>
2020-12-10 16:16:22 +0530

    Add files via upload
    Type   cmds   to get the commands!
    also subscribe to my Youtube channel link: https://www.youtube.com/channel/UCA_3t4pPc9GRIFiP86kRD2Q

---
+++ chatbot me.bat
@@ -0,0 +1,35 @@
+:s
+@echo off
+cls
+set/p chat=Enter Command:
+if %chat% == who_are_you goto who
+if %chat% == hello goto hi
+if %chat% == hi goto hi
+if %chat% == what_is_your_age goto age
+if %chat% == cmds goto cmd
+
+:who
+echo I am Sancho's Assistant!
+pause
+goto s
+
+:hi
+set/p name=
+pause
+echo Wow! What a nice name!
+pause
+goto s
+
+:age
+echo I do not have any age as I am ai!
+pause
+goto s
+
+:cmd
+echo who_are_you
+echo hello
+echo hi
+echo what_is_your_age
+echo cmds
+pause
+goto s
